ddba2483c2ea76335710723a3463b73bbb56755d
Your Name <mfaikoguz@gmail.com>
2020-07-16 22:07:51 -0500

    first commit

---
+++ src/test/java/com/nextBase/runners/CukesRunner.java
@@ -0,0 +1,15 @@
+package com.nextBase.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "com/nextBase/step_definitions"
+)
+public class CukesRunner {
+
+}
